7a121245427bdb93bdac8cbaa608677b6eead685
Guilherme Pereira <guilhermepereiraa95@gmail.com>
2022-06-17 09:10:10 -0300

    Corrigindo warnings

--- src/pages/Repos/Repos.tsx
+++ src/pages/Repos/Repos.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 import IPage from '../../interfaces/page';
 import { useLocation, useParams } from 'react-router-dom';
 import api from '../../services/api';
@@ -20,8 +20,10 @@ const Repo: React.FunctionComponent<IPage> = props => {
               response.data.map((r: any) => {
               let index = r['updated_at'].indexOf("T");
               let first = r['updated_at'].substr(0, index); // Gets the first part
-              r['updated_at'] = first;
-                })
+              let formated = first.replaceAll('-',',')
+              r['updated_at'] = formated;
+              return r;
+            })
               setRepos(response.data)
         }).catch((err) => {
             console.error("ops! ocorreu um erro" + err);
@@ -34,10 +36,10 @@ const Repo: React.FunctionComponent<IPage> = props => {
             <h4 className='text-center pt-3'><FiBook></FiBook> Repositórios <span className="border rounded-circle px-1 bg-light">{user.public_repos}</span> </h4>
             <section className="d-flex justify-content-center mt-5">
                 <div className="col-5">
-                   <img className="img-thumbnail rounded-circle" src={user.avatar_url} ></img>
+                   <img className="img-thumbnail rounded-circle" alt="Github" src={user.avatar_url} ></img>
                    <h3>{user.name}  </h3>
                    <p className="text-muted">{user.login}</p>
-                   <a className="button btn btn-success">Follow</a>
+                   <a className="button btn btn-success" href={user.url}>Follow</a>
                    <p><strong>@{user.login}</strong></p>
                    <p><FiUsers></FiUsers> {user.followers} <span className='text-muted'>followers </span>- {user.following} <span className='text-muted'>following</span> - <FiStar></FiStar> {user.public_gists}</p>
                    <p><FiMail></FiMail> {user.email ? user.email : 'E-mail não informado'}</p>
@@ -56,7 +58,6 @@ const Repo: React.FunctionComponent<IPage> = props => {
                                 <span className="spacing"><FiShare2></FiShare2> {items['forks_count']}</span>
                                 <span className="spacing">Atualizada em {items['updated_at']}</span>
                             </p>   
-                            {/* <p>{Intl.DateTimeFormat('en-GB', { dateStyle: 'full', timeStyle: 'long' }).format(items['updated_at'])}</p>   */}
                             
                             
                                                
